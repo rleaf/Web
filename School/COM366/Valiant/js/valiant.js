08caae7ef7f9bf69ed97e956d3543765abd47677
Rleaf <calcifer44@gmail.com>
2019-12-11 12:39:12 -0500

    v7.0 - Remove Grid - Brighter Red - Finish Contact section    - Begin implementing an on.click copy for email    - Ended up just putting his email there    - Pushed the footer a little higher

--- School/COM366/Valiant/js/valiant.js
+++ School/COM366/Valiant/js/valiant.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
       this.style.display = 'none';
    });
 
-   $(document).on('click', '.img-text', function() {
+   $('.gallery').on('click', '.img-text', function() {
       // console.log('atads');
       let imageSrc = $(this).parents('.col-md-4').find('img').attr('src');
       console.log(imageSrc);
@@ -32,24 +32,13 @@ $(document).ready(function() {
       modal.style.display = 'block';
       console.log('block');
    });
-});
 
-$(".contact-txt .copy").on("click", function(e) {
-   console.log('weeeeee');
-   e.preventDefault();
-   $(this).find(".email").select();
-   document.execCommand("copy");
-   TweenMax.set($(this).find("span"), {
-      y: 40,
-      autoAlpha: 0
+   $("a.copy").on("click", function(e) {
+      e.preventDefault();
+      console.log('weeeeee');
+      $(this).find(".email").select();
+      document.execCommand("copy");
+
    });
-   TweenMax.to($(this).find("span"), 2, {
-      ease: reactive,
-      y: 0,
-      autoAlpha: 1
-   });
-   TweenMax.to($(this).find("span"), 2, {
-      autoAlpha: 0,
-      delay: 2
-   });
+
 });
