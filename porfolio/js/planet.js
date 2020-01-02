50f0e83e581b3cebe8594872ab57da0b375c930d
Rleaf <calcifer44@gmail.com>
2020-01-02 01:37:15 -0500

    using loops > typing out each new ring variable

--- porfolio/js/planet.js
+++ porfolio/js/planet.js
@@ -3,64 +3,54 @@
    let scene = new THREE.Scene();
    let camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
    let renderer = new THREE.WebGLRenderer({antialias: false});
+   let ringGeometry = [];
+   let circleRad = 1.5;
 
    renderer.setSize(window.innerWidth, window.innerHeight);
    document.body.appendChild(renderer.domElement);
 
-   // material
-   let material = new THREE.MeshBasicMaterial({color: '#ffffff'});
-   let sunMaterial = new THREE.PointsMaterial({color: '#d3c124', size: 0.015});
-   let particleMaterial = new THREE.PointsMaterial({color: '#ffffff',size: 0.01});
-   let testParticleMaterial = new THREE.PointsMaterial({color: 'red',size: 0.01});
    
    // geometry
-   let circleGeometry = new THREE.CircleGeometry(3.2, 100);
+   for (let i = 0; i < 7; i++) {
+      ringGeometry[i] = new THREE.CircleGeometry(circleRad, 100);
+      circleRad = circleRad + 0.5;
+   };
+
    let sphere = new THREE.SphereBufferGeometry(0.5, 25, 25);
-   let r = new THREE.TorusBufferGeometry(1.5, .18, 2, 150);
-   let rr = new THREE.TorusBufferGeometry(2.25, .19, 2, 200);
-   let rrr = new THREE.TorusBufferGeometry(3, .21, 2, 100);
-   let rrrr = new THREE.TorusBufferGeometry(6, 1.5, 2, 100);
+   ringGeometry[0].vertices.shift();
    
-   // sphere.rotateX(0.41);
-   // r.rotateX(1.9808);
-   // rr.rotateX(1.9808);
-   // rrr.rotateX(1.9808);
-   // rrrr.rotateX(1.9808);
+   // material
+   let sunMaterial = new THREE.PointsMaterial({color: '#d3c124', size: 0.015});
+   let particleMaterial = new THREE.PointsMaterial({color: '#ffffff',size: 0.01});
+   
 
-   
-   
-   let circle = new THREE.Line(circleGeometry, new THREE.LineDashedMaterial({color: '#ffffff'}));
-
-   // lines 
+   // structures -- composed of (geometry, material); 
    let sphereParticle = new THREE.Points(sphere, sunMaterial);
-   let rParticle = new THREE.Points(r, particleMaterial);
-   let rrParticle = new THREE.Points(rr, particleMaterial);
-   let rrrParticle = new THREE.Points(rrr, particleMaterial);
-   let rrrrParticle = new THREE.Points(rrrr, particleMaterial);
+   let ringStructure = [];
 
-   let axis = new THREE.Vector3(0, 1, 0).normalize();
+   for (let i = 0; i < ringGeometry.length; i++) {
+      ringStructure[i] = new THREE.Points(ringGeometry[i], particleMaterial);
+      ringStructure[i].rotateX(1.9808);
+      scene.add(ringStructure[i]);
+   }
 
-   let rings = new THREE.Group();
-   rings.add(rParticle);
-   rings.add(rrParticle);
-   rings.add(rrrParticle);
-   rings.add(rrrrParticle);
+   let xAxis = new THREE.Vector3(1, 0, 0).normalize();
+   let yAxis = new THREE.Vector3(0, 1, 0).normalize();
+   let zAxis = new THREE.Vector3(0, 0, 1).normalize();
+
 
    sphereParticle.rotateX(0.41);
-   rings.rotateX(1.9808);
 
    scene.add(sphereParticle);
-   scene.add(rings);
-
    camera.position.z = 6;
 
    function animate() {
 
-
-      rings.rotateOnAxis(axis, 0.002);
-      // rings.rotateOnWorldAxis(axis, 0.002);
-       sphereParticle.rotateOnAxis(axis, 0.002);
-       sphereParticle.rotateOnWorldAxis(axis, 0.002);
+      for (let i = 0; i < ringGeometry.length; i++) {
+         ringStructure[i].rotateOnWorldAxis(yAxis, 0.002);
+      }
+      sphereParticle.rotateOnAxis(yAxis, 0.008);
+      sphereParticle.rotateOnWorldAxis(yAxis, 0.002);
 
 
       requestAnimationFrame(animate);
