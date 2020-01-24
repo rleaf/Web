60e3920789f8f8686ea69c3110b98ffcec4125a0
Rleaf <calcifer44@gmail.com>
2020-01-23 21:20:35 -0500

    sketches _

--- portfolio/vue_ptf/src/pages/sketches.vue
+++ portfolio/vue_ptf/src/pages/sketches.vue
@@ -1,23 +1,34 @@
 <template>
    <div id="landing">
       <NavBar/>
-      <construction/>
+      <div id="textContainerHeader">
+      </div>
+         <div id="curriculumStructure">
+            <div id="curriculumBody">
+               <div id="curriculumHeader">
+                  <u style="font-size: 18px;">What's this?</u>
+               </div>
+                  <p>
+                     Everything and anything completed or not,
+                     that I feel does not warrant it's own page but is worth a mention 
+                     will be put here.
+                  </p>
+            </div>
+         </div>
 
    </div>
 </template>
 
 <script>
 import NavBar from '../components/nav.vue'
-import construction from '../components/construction.vue'
 
 export default {
    components: {
       NavBar,
-      construction
    }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
